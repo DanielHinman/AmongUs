8cc5dcdd74f9ee8779429cfb50f8ea0a02e5e69f
Basem A <32045565+Seabassem@users.noreply.github.com>
2020-11-05 18:17:41 -0500

    Adding Unity Files
    Game files with character movement and animation

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+
+    public Animator animator;
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Animator
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        //Init axis
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
+
+        //Movement
+        transform.position = transform.position + horizontal * Time.deltaTime * horizontalSpeed;
+        transform.position = transform.position + vertical * Time.deltaTime * verticalSpeed;
+        
+    }
+}
